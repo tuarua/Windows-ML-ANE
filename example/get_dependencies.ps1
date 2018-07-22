cfdf68057b1dbe567b1c2f970347dcc3862031b9
Eoin Landy <el111@yahoo.com>
2018-07-22 23:10:30 +0100

    no message

--- example/get_dependencies.ps1
+++ example/get_dependencies.ps1
@@ -1,4 +1,4 @@
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://github.com/tuarua/Windows-ML-ANE/releases/download/0.0.2/MLANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\MLANE.ane"
-Invoke-WebRequest -Uri https://github.com/tuarua/Windows-ML-ANE/releases/download/0.0.2/GoogLeNetPlaces.onnx?raw=true -OutFile "$currentDir\src\GoogLeNetPlaces.onnx"
+Invoke-WebRequest -Uri https://github.com/tuarua/Windows-ML-ANE/releases/download/0.0.3/MLANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\MLANE.ane"
+Invoke-WebRequest -Uri https://github.com/tuarua/Windows-ML-ANE/releases/download/0.0.3/GoogLeNetPlaces.onnx?raw=true -OutFile "$currentDir\src\GoogLeNetPlaces.onnx"
