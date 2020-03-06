328127c24443c38e62e815dbb3ff00c54892826a
Eoin Landy <el111@yahoo.com>
2020-03-06 19:29:18 +0000

    no message

--- example/get_dependencies.ps1
+++ example/get_dependencies.ps1
@@ -1,5 +1,5 @@
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://github.com/tuarua/Windows-ML-ANE/releases/download/0.0.9/MLANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\MLANE.ane"
-Invoke-WebRequest -Uri https://github.com/tuarua/FreSharp/releases/download/2.3.0/FreSharp.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\FreSharp.ane"
-Invoke-WebRequest -Uri https://github.com/tuarua/Windows-ML-ANE/releases/download/0.0.9/SqueezeNet.onnx?raw=true -OutFile "$currentDir\src\SqueezeNet.onnx"
+Invoke-WebRequest -Uri https://github.com/tuarua/Windows-ML-ANE/releases/download/0.1.0/MLANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\MLANE.ane"
+Invoke-WebRequest -Uri https://github.com/tuarua/FreSharp/releases/download/2.4.0/FreSharp.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\FreSharp.ane"
+Invoke-WebRequest -Uri https://github.com/tuarua/Windows-ML-ANE/releases/download/0.1.0/SqueezeNet.onnx?raw=true -OutFile "$currentDir\src\SqueezeNet.onnx"
